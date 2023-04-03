2114d1300378db8d5798808d171989f0e88d72ba
deergod <CommonTroll@live.co.uk>
2023-04-03 10:30:00 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+    const [staff, setStaff] = useState([]);
+
+
+
     return (
         <div className="App">
-            
+
         </div>
     );
 }
