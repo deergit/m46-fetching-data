37564fba2d45171d8e38dc4a35e8f5d2ffb81457
deergod <CommonTroll@live.co.uk>
2023-04-03 11:15:21 +0100

    updating to follow example

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-    const [staff, setStaff] = useState([]);
+    const [allCharacters, setAllCharacters] = useState([]);
 
-
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch("http://legacy--api.herokuapp.com/api/v1/characters");
+            console.log(response);
+        }
+        fetchData();
+    }, [])
 
     return (
         <div className="App">
-
+            <h1>Fetching Data</h1>
         </div>
     );
 }
