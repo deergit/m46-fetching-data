a9f08c306c42874add934c89f190a03b360ad6ab
deergod <CommonTroll@live.co.uk>
2023-04-03 13:04:47 +0100

    reworking into art website

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-    const [allCharacters, setAllCharacters] = useState([]);
+    const [allArt, setAllArt] = useState([]);
+    const [errorMsg, setErrorMsg] = useState(null);
 
     useEffect(() => {
         const fetchData = async () => {
-            const response = await fetch("http://legacy--api.herokuapp.com/api/v1/characters");
-            const data = await response.json();
-            setAllCharacters(data);
+            try {
+                const response = await fetch("https://api.artic.edu/api/v1/artworks");
+                
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                const data = await response.json();
+                setAllArt(data.data);
+            } catch(err) {
+                setErrorMsg("Data not found");
+                console.log(err.message);
+            }
         }
         fetchData();
     }, [])
@@ -16,9 +26,22 @@ function App() {
     return (
         <div className="App">
             <h1>Fetching Data</h1>
-            {allCharacters.map((character, index) => {
-                return <p key={index}>{character.name}</p>
-            })}
+            {errorMsg && <h3>{errorMsg}</h3>}
+            <div id="container">
+                {allArt.map((piece, index) => {
+                    return (
+                        <div key={index} className="content-wrapper">
+                            <div className="image">
+                                <img src={`https://www.artic.edu/iiif/2/${piece.image_id}/full/843,/0/default.jpg`} alt={piece.thumbnail.alt_text} draggable="false"></img>
+                            </div>
+                            <div className="info">
+                                <h2>{piece.title}</h2>
+                                <h3>{piece.artist_title}, {piece.medium_display}</h3>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
