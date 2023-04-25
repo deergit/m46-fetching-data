dc9949112bb1bd5f3220dca5393c4d77b7a515d8
deergod <CommonTroll@live.co.uk>
2023-04-25 13:36:26 +0100

    updating formatting

--- src/App.js
+++ src/App.js
@@ -5,63 +5,63 @@ import Gallery from "./pages/Gallery";
 import "./App.css";
 
 const App = () => {
-    const [allArt, setAllArt] = useState([]);
-    const [errorMsg, setErrorMsg] = useState(null);
-    const [currentPiece, setCurrentPiece] = useState({});
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [bgBlur, setBGBlur] = useState(0);
+  const [allArt, setAllArt] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [currentPiece, setCurrentPiece] = useState({});
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [bgBlur, setBGBlur] = useState(0);
 
-    const handleShow = (piece) => {
-        setCurrentPiece(piece);
-        setIsOpen(true);
-        setBGBlur(10);
-    }
+  const handleShow = (piece) => {
+    setCurrentPiece(piece);
+    setIsOpen(true);
+    setBGBlur(10);
+  }
 
-    const handleClose = () => {
-        setIsOpen(false);
-        setBGBlur(0);
-    }
+  const handleClose = () => {
+    setIsOpen(false);
+    setBGBlur(0);
+  }
 
-    useEffect(() => {
-        const dataCache = JSON.parse(localStorage.getItem("dataCache"));
+  useEffect(() => {
+    const dataCache = JSON.parse(localStorage.getItem("dataCache"));
 
-        if (dataCache && dataCache.length > 0) {
-            setAllArt(dataCache);
-        } else {
-            const fetchData = async () => {
-                try {
-                    const response = await fetch("https://api.artic.edu/api/v1/artworks");
+    if (dataCache && dataCache.length > 0) {
+      setAllArt(dataCache);
+    } else {
+      const fetchData = async () => {
+        try {
+          const response = await fetch("https://api.artic.edu/api/v1/artworks");
 
-                    if (!response.ok) {
-                        throw new Error(response.statusText);
-                    }
-                    const data = await response.json();
-                    setAllArt(data.data);
-                    localStorage.setItem("dataCache", JSON.stringify(data.data));
-                } catch(err) {
-                    setErrorMsg("ERROR: unable to retrieve data from artic API");
-                    console.log(err.message);
-                }
-            }
-            fetchData();
+          if (!response.ok) {
+            throw new Error(response.statusText);
+          }
+          const data = await response.json();
+          setAllArt(data.data);
+          localStorage.setItem("dataCache", JSON.stringify(data.data));
+        } catch (err) {
+          setErrorMsg("ERROR: unable to retrieve data from artic API");
+          console.log(err.message);
         }
-    }, []);
+      }
+      fetchData();
+    }
+  }, []);
 
 
-    return (
-        <BrowserRouter>
-            <nav>
-                <h1>Art Institute of Chicago API</h1>
-                <Link to="/">Home</Link>
-                <Link to="/gallery">Gallery</Link>
-            </nav>
+  return (
+    <BrowserRouter>
+      <nav>
+        <h1>Art Institute of Chicago API</h1>
+        <Link to="/">Home</Link>
+        <Link to="/gallery">Gallery</Link>
+      </nav>
 
-            <Routes>
-                <Route path="/" element={ <Home /> } />
-                <Route path="/gallery" element={ <Gallery allArt={allArt} modalIsOpen={modalIsOpen} handleShow={handleShow} handleClose={handleClose} currentPiece={currentPiece} bgBlur={bgBlur} errorMsg={errorMsg} /> } />
-            </Routes>
-        </BrowserRouter>
-    );
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/gallery" element={<Gallery allArt={allArt} modalIsOpen={modalIsOpen} handleShow={handleShow} handleClose={handleClose} currentPiece={currentPiece} bgBlur={bgBlur} errorMsg={errorMsg} />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -3,28 +3,28 @@ import { Wrapper, Container, ArtPiece } from "./Gallery.styles";
 import Modal from "../components/Modal";
 
 const Gallery = (props) => {
-    return (
-        <div className="Page">
-            {props.modalIsOpen ? <Modal closeModal={props.handleClose} piece={props.currentPiece} hidden /> : null}
-            <Wrapper onClick={props.modalIsOpen ? props.handleClose : null} blur={props.bgBlur}>
-                {props.errorMsg && <h3>{props.errorMsg}</h3>}
+  return (
+    <div className="Page">
+      {props.modalIsOpen ? <Modal closeModal={props.handleClose} piece={props.currentPiece} hidden /> : null}
+      <Wrapper onClick={props.modalIsOpen ? props.handleClose : null} blur={props.bgBlur}>
+        {props.errorMsg && <h3>{props.errorMsg}</h3>}
 
-                <Container>
-                    {props.allArt.map((piece, index) => {
-                        return (
-                            <ArtPiece key={index} className="content-wrapper" onClick={() => props.handleShow(piece)}>
-                                <img src={`https://www.artic.edu/iiif/2/${piece.image_id}/full/843,/0/default.jpg`} alt={`${piece.title} by ${piece.artist_title} in ${piece.medium_display}`} draggable="false"></img>
-                                <div className="info">
-                                    <h2>{piece.title}</h2>
-                                    <Textfit mode="multi" min={10} max={70}>{piece.artist_title}, {piece.medium_display}</Textfit>
-                                </div>
-                            </ArtPiece>
-                        );
-                    })}
-                </Container>
-            </Wrapper>
-        </div>
-    );
+        <Container>
+          {props.allArt.map((piece, index) => {
+            return (
+              <ArtPiece key={index} className="content-wrapper" onClick={() => props.handleShow(piece)}>
+                <img src={`https://www.artic.edu/iiif/2/${piece.image_id}/full/843,/0/default.jpg`} alt={`${piece.title} by ${piece.artist_title} in ${piece.medium_display}`} draggable="false"></img>
+                <div className="info">
+                  <h2>{piece.title}</h2>
+                  <Textfit mode="multi" min={10} max={70}>{piece.artist_title}, {piece.medium_display}</Textfit>
+                </div>
+              </ArtPiece>
+            );
+          })}
+        </Container>
+      </Wrapper>
+    </div>
+  );
 }
 
 export default Gallery
